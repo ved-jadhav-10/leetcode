314c7f115949cd5999c061114cdafbf8d3fac7e8
Ved Jadhav <ved.amit.jadhav@gmail.com>
2026-09-01 22:53:09 +0530

    Time: 7 ms (18.28%) | Memory: 9.6 MB (75.49%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        else
+        {
+            sort(s.begin(), s.end());
+            sort(t.begin(), t.end());
+            if (s == t)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+};
