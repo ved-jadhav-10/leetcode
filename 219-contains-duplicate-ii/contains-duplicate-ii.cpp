fba4525e3164846697b7f85c5d5b982914e35267
Ved Jadhav <ved.amit.jadhav@gmail.com>
2026-09-05 01:34:33 +0530

    Time: 115 ms (36.46%) | Memory: 134 MB (28.52%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <unordered_set>
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(std::vector<int>& nums, int k) 
+    {
+        unordered_set<int> set;
+        
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (i > k)
+            {
+                set.erase(nums[i - k - 1]);
+            }
+
+            if (set.count(nums[i]))
+            {
+                return true;
+            }
+            set.insert(nums[i]);
+        }
+        return false;
+    }
+};
