a59184633faeb412a447d2274bd81c2665726b5c
Ved Jadhav <ved.amit.jadhav@gmail.com>
2026-09-12 23:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (33.29%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+        int count = 0;
+
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        // Count last word
+        while (i >= 0 && s[i] != ' ') {
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+};
