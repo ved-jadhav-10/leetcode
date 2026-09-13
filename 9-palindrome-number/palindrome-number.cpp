566bd9015c7e312e84380f51674f15a105d9f2d0
Ved Jadhav <ved.amit.jadhav@gmail.com>
2026-09-13 19:33:18 +0530

    Time: 3 ms (43.80%) | Memory: 8.5 MB (64.70%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,24 +3,17 @@ public:
     bool isPalindrome(int x) 
     {
         if (x < 0)
-        {
             return false;
-        }
 
-        long compare = x;
-        long z = 0;
-        long temp = 0;
+        int compare = x;
+        long long z = 0;
+
         while (compare != 0)
         {
-            temp = compare % 10;
-            compare = compare / 10;
-            z = z * 10 + temp;
+            z = z * 10 + compare % 10;
+            compare /= 10;
         }
 
-        if (z == x)
-        {
-            return true;
-        }
-        return false;
+        return z == x;
     }
 };
